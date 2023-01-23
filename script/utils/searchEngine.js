9aba106613e8a2b27ff3593180b8c57205e8f100
Aymerick Toonba <godzilla.toonba@gmail.com>
2023-01-23 15:36:15 +0100

    updated code to have a version with array methode

--- script/utils/searchEngine.js
+++ script/utils/searchEngine.js
@@ -1,3 +1,15 @@
+// conseil de chatGPT pour améliorer mon code
+
+// Séparer les fonctions logiques dans des fichiers séparés : pour cela tu peux créer un nouveau fichier pour chaque fonction logique (par exemple, un fichier mainSearch.js pour la fonction mainSearch) et y déplacer le code de cette fonction. Ensuite, dans le fichier principal (searchEngine.js), tu peux importer ces fonctions à l'aide de l'instruction import de JavaScript.
+
+// Utiliser des noms de variables plus explicites : tu peux changer les noms de variables pour qu'ils soient plus explicites (par exemple, recipesArray pour remplacer array dans la fonction mainSearch).
+
+// Utiliser des commentaires pour expliquer le code : tu peux ajouter des commentaires pour expliquer ce que font les différentes parties de ton code. Il est important de les mettre à des endroits pertinents pour que les personnes qui lisent ton code puissent comprendre ce que tu essaies de faire.
+
+// Utiliser des constantes plutôt que des variables pour les éléments du DOM : tu peux remplacer toutes les variables qui stockent des éléments du DOM par des constantes, pour éviter les erreurs potentielles.
+
+// Utiliser des fonctions pour répéter des actions similaires : tu peux créer une fonction displaySuggestion pour afficher les suggestions pour les ingrédients, les appareils et les ustensiles. Cette fonction prend en paramètre les données à afficher et le type de suggestion (ingrédients, appareils ou ustensiles) et se charge de l'affichage.
+
 // DOM Element
 const mainSearchInput = document.getElementById('search')
 const recipesSection = document.getElementsByClassName('recipes-gallery')
@@ -12,12 +24,7 @@ let myAdvancedInput = [ingredientsInput, appareilsInput, ustensilsInput]
 
 // Function to get an array without any element repeted
 function getListOfUnique(array) {
-  let newArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (array.indexOf(array[i]) === i) {
-      newArray.push(array[i])
-    }
-  }
+  let newArray = array.filter((element, index) => array.indexOf(element) === index)
   return newArray
 }
 
@@ -32,15 +39,15 @@ function getSuggestionList(array) {
   listUniqueAppareils = []
   let listAllUstensils = []
   listUniqueUstensils = []
-  for (let element of array) {
+  array.forEach((element) => {
     listAllAppareils.push(element.appliance.toLowerCase())
-    for (let ingredient of element.ingredients) {
+    element.ingredients.forEach((ingredient) => {
       listAllIngredients.push(ingredient.ingredient.toLowerCase())
-    }
-    for (let ustensil of element.ustensils) {
+    })
+    element.ustensils.forEach((ustensil) => {
       listAllUstensils.push(ustensil.toLowerCase())
-    }
-  }
+    })
+  })
   listUniqueIngredients = getListOfUnique(listAllIngredients)
   listUniqueAppareils = getListOfUnique(listAllAppareils)
   listUniqueUstensils = getListOfUnique(listAllUstensils)
@@ -50,102 +57,53 @@ function getSuggestionList(array) {
 }
 
 function mainSearch(input, array) {
-  let currentSearch = ''
-  let currentRecipes = []
-  if (input.value.length < 3) {
-    currentRecipes = recipes
-  } else if (input.value.length >= 3) {
-    currentSearch = input.value.toLowerCase()
-    for (let i = 0; i < array.length; i++) {
-      if (array[i].name.toLowerCase().includes(currentSearch)) {
-        currentRecipes.push(array[i])
-      } else if (
-        array[i].description
+  const currentSearch = input.value.toLowerCase()
+  let currentRecipes = array
+  if (currentSearch.length >= 3) {
+    currentRecipes = array.filter(
+      (recipe) =>
+        recipe.name.toLowerCase().includes(currentSearch) ||
+        recipe.description
           .toLowerCase()
           .replace(/[,.:;]/g, ' ')
-          .includes(currentSearch)
-      ) {
-        currentRecipes.push(array[i])
-      }
-      for (let j = 0; j < array[i].ingredients[j].length; j++) {
-        if (array[i].ingredients[j].ingredient.toLowerCase().includes(currentSearch)) {
-          currentRecipes.push(array[i])
-        }
-      }
-    }
+          .includes(currentSearch) ||
+        recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(currentSearch))
+    )
   }
   displayRecipes(currentRecipes)
   getSuggestionList(currentRecipes)
   hiddingSuggestion()
   if (currentRecipes.length === 0) {
     let error = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc'
-    console.log('coucou')
     let p = document.createElement('p')
     p.textContent = error
     recipesSection[0].innerHTML = ''
-    console.log(recipesSection[0])
     recipesSection[0].appendChild(p)
   }
   return currentRecipes
 }
 
 function getMatchingRecipe(arrayToTest, previousMatchingRecipes, recipeProperty) {
-  for (let element of arrayToTest) {
-    newArray = []
-    for (let recipe of previousMatchingRecipes) {
-      for (let i = 0; i < recipe[recipeProperty].length; i++) {
-        if (recipeProperty === 'ingredients') {
-          if (element === recipe[recipeProperty][i].ingredient.toLowerCase()) {
-            newArray.push(recipe)
-          }
-        } else {
-          if (element === recipe[recipeProperty][i].toLowerCase()) {
-            newArray.push(recipe)
-          }
-        }
-      }
-    }
-    previousMatchingRecipes = newArray
-  }
-  return newArray
+  return arrayToTest.reduce((newArray, element) => {
+    previousMatchingRecipes = newArray.filter((recipe) => recipe[recipeProperty].some((prop) => (recipeProperty === 'ingredients' ? prop.ingredient.toLowerCase() === element : prop.toLowerCase() === element)))
+    return previousMatchingRecipes
+  }, previousMatchingRecipes)
 }
 
 function advancedSearch(tag) {
   let currentRecipes = mainSearch(mainSearchInput, recipes)
-  let advancedRecipesAppareils = []
-  let advancedRecipesIngredients = []
-  let advancedRecipesUstensils = []
-  if (tag.appareils.length === 0) {
-    advancedRecipesAppareils = currentRecipes
-  } else {
-    for (let recipe of currentRecipes) {
-      if (tag.appareils[0] === recipe.appliance.toLowerCase()) {
-        advancedRecipesAppareils.push(recipe)
-      }
-    }
-  }
-  if (tag.ingredients.length === 0) {
-    advancedRecipesIngredients = advancedRecipesAppareils
-  } else {
-    advancedRecipesIngredients = getMatchingRecipe(tag.ingredients, advancedRecipesAppareils, 'ingredients')
-  }
-  if (tag.ustensils.length === 0) {
-    advancedRecipesUstensils = advancedRecipesIngredients
-  } else {
-    advancedRecipesUstensils = getMatchingRecipe(tag.ustensils, advancedRecipesIngredients, 'ustensils')
-  }
+  let advancedRecipesAppareils = tag.appareils.length === 0 ? currentRecipes : currentRecipes.filter((recipe) => recipe.appliance.toLowerCase() === tag.appareils[0])
+  let advancedRecipesIngredients = tag.ingredients.length === 0 ? advancedRecipesAppareils : getMatchingRecipe(tag.ingredients, advancedRecipesAppareils, 'ingredients')
+  let advancedRecipesUstensils = tag.ustensils.length === 0 ? advancedRecipesIngredients : getMatchingRecipe(tag.ustensils, advancedRecipesIngredients, 'ustensils')
+  console.log(advancedRecipesIngredients)
+  console.log(advancedRecipesUstensils)
   getSuggestionList(advancedRecipesUstensils)
   displayRecipes(advancedRecipesUstensils)
 }
 
 function advancedSearchTag(input, array, DOM, type) {
   const currentAdvancedSearch = input.value.toLowerCase()
-  let currentSuggestion = []
-  for (let element of array) {
-    if (element.toLowerCase().includes(currentAdvancedSearch)) {
-      currentSuggestion.push(element)
-    }
-  }
+  let currentSuggestion = array.filter((element) => element.toLowerCase().includes(currentAdvancedSearch))
   DOM.innerHTML = ''
   if (currentSuggestion.length === 0) {
     DOM.innerHTML = 'Aucune suggestion ne correspond à votre recherche'
